f90c5810da47ec707a5a682b3b2f10e8fb4f80b8
Matthew Conley <mconley@teslamotors.com>
2015-08-19 10:22:15 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+config = {
+    'description': 'systool is a library for system manipulation',
+    'author': 'Matthew Conley',
+    'url': 'https://github.com/mtconley/systools',
+    'download_url': 'https://github.com/mtconley/systools.git.',
+    'author_email': '',
+    'version': '0.0.1',
+    'install_requires': [ ],
+    'packages': find_packages(),
+    'name': 'systools'
+}
+
+print "system is: " + sys.platform
+print ''
+print "installing dependencies... "
+print config['install_requires']
+
+setup(**config)
