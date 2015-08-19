8c298a435b73ac69a400cddb536b5308747da45b
Matthew Conley <mconley@teslamotors.com>
2015-08-19 11:37:08 -0700

    add Registery object for variable persistence

--- systools/misc.py
+++ systools/misc.py
@@ -21,3 +21,47 @@ def isboolswitch(pattern):
     """Identify if list is [True, False]
     """
     return isbooliter(pattern) and len(pattern)==2 and sum(pattern) == 1
+
+
+class Registry(object):
+    __registry = {}
+    def __init__(self, *args):
+        """Register a set of variables to restore at a later time in the session
+
+        Parameters
+        ----------
+            args : `tuple`
+                args is a tuple of tuples.  Each tuple in args will be used to
+                make the registery dictionary
+
+        Example
+        -------
+        >>> restore = Registry(('ipout', sys.stdout), ('iperr', sys.stderr))
+        >>> restore.ipout
+            <open file '<stdout>', mode 'w' at 0x10028e150>
+        >>> restore.add('test', 'response')
+        >>> restore
+            Registered Variables:
+                test
+                ipout
+                iperr
+        >>> restore.remove('test')
+        >>> restore
+            Registered Variables:
+                ipout
+                iperr
+        """
+        for arg in args:
+            self.add(arg)
+
+    def add(self, pair):
+        key, value = pair
+        self.__registry[key] = self.__registry.get(key, value)
+        self.__dict__.update(self.__registry)
+
+    def remove(self, key):
+        self.__registry.pop(key)
+        self.__dict__.pop(key)
+
+    def __repr__(self):
+        return 'Registered Variables:\n\t'+ '\n\t'.join(self.__registry.keys())
--- systools/prompt.py
+++ systools/prompt.py
@@ -8,21 +8,21 @@ def prompt_options(pattern, prompt):
     """Ensure response to prompt matches pattern.
     If pattern is list, literal options will be displayed"""
     if isboolswitch(pattern):
-        value = prompt_bool(prompt)
+        value = _prompt_bool(prompt)
     elif isinstance(pattern, list):
-        value = list_prompt(pattern, prompt)
+        value = _list_prompt(pattern, prompt)
     else:
-        value = pattern_prompt(pattern, prompt)
+        value = _pattern_prompt(pattern, prompt)
     return value
 
-def prompt_bool(prompt):
+def _prompt_bool(prompt):
     prompt = "{0} [True/False]: ".format(prompt)
     menu_options = {'t': True, 'f': False}
     choice = raw_input(prompt) or 'f'
     value = menu_options.get(choice.lower()[0], False)
     return value
 
-def pattern_prompt(pattern, prompt):
+def _pattern_prompt(pattern, prompt):
     ERROR = '\nInput must match pattern, {0}\n'.format(pattern)
     switch = True    
     while switch:
@@ -32,7 +32,7 @@ def pattern_prompt(pattern, prompt):
             print ERROR
     return value
 
-def list_prompt(menu_options, prompt):
+def _list_prompt(menu_options, prompt):
     prompt = prompt + '\n  ' + '\n  '.join(menu_options)
     pattern = '|'.join(menu_options)
     ERROR = '\nInput must be in list\n'
